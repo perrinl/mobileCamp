9f29b036424d596c6c8306e85be96084e3cb2876
perrinl <perrinl@users.noreply.github.com>
2017-05-05 08:56:39 +0200

    dhuwehf

--- src/providers/doctor-sercice.ts
+++ src/providers/doctor-sercice.ts
@@ -12,28 +12,28 @@ export class DoctorService {
   getSpecialities() {
     let headers = new Headers({ 'Authorization': localStorage.getItem('token') });
     let options = new RequestOptions({ headers: headers });
-    return this.http.get("http://localhost:3000/doctor-types", options)
+    return this.http.get("http://172.16.29.62:3000/doctor-types", options)
         .map(response => response.json());
   }
 
   ajouter(doctor: {lastName:string, phone:string, address:string, type_id:number}) {
     let headers = new Headers({ 'Authorization': localStorage.getItem('token'), 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
-    return this.http.post("http://localhost:3000/users/"+localStorage.getItem('id')+"/doctors", JSON.stringify(doctor), options)
+    return this.http.post("http://172.16.29.62:3000/users/"+localStorage.getItem('id')+"/doctors", JSON.stringify(doctor), options)
         .map(response => response.json());
   }
 
   getDoctors() {
     let headers = new Headers({ 'Authorization': localStorage.getItem('token') });
     let options = new RequestOptions({ headers: headers });
-    return this.http.get("http://localhost:3000/users/"+localStorage.getItem('id')+"/doctors", options)
+    return this.http.get("http://172.16.29.62:3000/users/"+localStorage.getItem('id')+"/doctors", options)
         .map(response => response.json());
   }
 
   deleteDoctor(id:number) {
     let headers = new Headers({ 'Authorization': localStorage.getItem('token') });
     let options = new RequestOptions({ headers: headers });
-    return this.http.delete("http://localhost:3000/users/"+localStorage.getItem('id')+"/doctors/"+id, options)
+    return this.http.delete("http://172.16.29.62:3000/users/"+localStorage.getItem('id')+"/doctors/"+id, options)
         .map(response => response.json());
   }
 }
